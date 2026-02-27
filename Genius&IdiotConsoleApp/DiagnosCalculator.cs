b59c52f226955cac0a3fa445fd25a95db891a209
usasm <usasmen123@mail.ru>
2026-02-27 10:40:46 +0300

    TODO: add JSON, winForms

---
+++ Genius&IdiotConsoleApp/DiagnosCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genius___Idiot
+{
+    internal class DiagnosCalculator
+    {
+        public static string MakeADiagnos(int questionsCount, int correctCount)
+        {
+            List<string> diagnosises = new List<string>() { "Идиот", "Бездарь", "Живчик", "Нормис", "Сигмантей", "Гений" };
+            double correctPercent = (double)correctCount / questionsCount * 100;
+            if (correctPercent == 0)
+                return diagnosises[0];
+            else if (correctPercent < 20)
+                return diagnosises[1];
+            else if (correctPercent < 40)
+                return diagnosises[2];
+            else if (correctPercent < 60)
+                return diagnosises[3];
+            else if (correctPercent < 80)
+                return diagnosises[4];
+            return diagnosises[5];
+        }
+    }
+}
