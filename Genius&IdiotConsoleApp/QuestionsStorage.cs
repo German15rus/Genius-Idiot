2b9ffbc904ff85f31d2d76c8b6f3184d9e5a14b5
German <usasmen123@mail.ru>
2026-03-04 23:39:06 +0300

    чето-чето но ниче не сделал

--- Genius&IdiotConsoleApp/QuestionsStorage.cs
+++ Genius&IdiotConsoleApp/QuestionsStorage.cs
@@ -1,43 +1,36 @@
-﻿namespace Genius___Idiot
+﻿using static System.Net.Mime.MediaTypeNames;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Genius___Idiot
 {
-	public class QuestionsStorage
-	{
-		private string questionsBankPath = "..\\..\\..\\questionsBank.json";
+    public class QuestionsStorage
+    {
+        private string questionsBankPath = "..\\..\\..\\questionsBank.json";
 
-  //      private string textsPath = "..\\..\\..\\Questions.txt";
-		//private string answersPath = "..\\..\\..\\Answers.txt";
+        public List<Question> GetAll()
+        {
+            if (string.IsNullOrWhiteSpace(File.ReadAllText(questionsBankPath)) == false)
+            {
+                GetDefaultQuestions();
+                return GetAll();
+            }
+            string json = File.ReadAllText(questionsBankPath);
 
-		public List<Question> GetAll()
-		{
-			string jsonContent = File.ReadAllText(questionsBankPath);
-			return System.Text.Json.JsonSerializer.Deserialize<List<Question>>(jsonContent) ?? new List<Question>();
-
-
-
-            //string[] questionsTexts = File.ReadAllLines(textsPath);
-            //string[] questionsAnswers = File.ReadAllLines(answersPath);
-            //List<Question> questions = new List<Question>();
-            //for (int i = 0; i < questionsTexts.Length; i++)
-            //{
-            //	Question question = new Question();
-            //	question.Text = questionsTexts[i];
-            //	question.Answer = questionsAnswers[i];
-            //	questions.Add(question);
-            //}
-            //return questions;
+            return System.Text.Json.JsonSerializer.Deserialize<List<Question>>(json) ?? new List<Question>();
         }
 
-		public void Add(Question question)
-		{
+        public void Add(Question question)
+        {
             var questions = GetAll();
             questions.Add(question);
             Save(questions);
-		}
+        }
 
-		public void Remove(int questionIndex)
-		{
+        public void Remove(int questionNum)
+        {
             var questions = GetAll();
-            questionIndex = questionIndex - 1;
+            int questionIndex = questionNum - 1;
 
             if (questionIndex >= 0 && questionIndex < questions.Count)
             {
@@ -46,34 +39,23 @@
             }
 
             questionIndex = questionIndex - 1;
-            
-   //         var withoutDel = File.ReadAllLines(textsPath).ToList();
-			//withoutDel.RemoveAt(questionIndex);
-			//File.WriteAllLines(textsPath, withoutDel);
-			
-			//withoutDel = File.ReadAllLines(answersPath).ToList();
-			//withoutDel.RemoveAt(questionIndex);
-			//File.WriteAllLines(answersPath, withoutDel);
-		}
+        }
 
-		public void Save(List<Question> questions)
-		{
-            string jsonContent = System.Text.Json.JsonSerializer.Serialize(questions, new System.Text.Json.JsonSerializerOptions
+        public void Save(List<Question> questions)
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(questions);
+            File.WriteAllText(questionsBankPath, json);
+        }
+        public void GetDefaultQuestions()
+        {
+            var defaultQuestions = new List<Question>
             {
-                WriteIndented = true
-            });
-            File.WriteAllText(questionsBankPath, jsonContent);
+                new Question { Text = "2+2?", Answer = "4" },
+                new Question { Text = "Столица Франции?", Answer = "Париж" },
+                new Question { Text = "Язык программирования?", Answer = "C#" }
+            };
 
-   //         List<string> questionsTexts = new List<string>();
-			//List<string> questionsAnswers = new List<string>();
-			//for (int i = 0; i < questions.Count; i++)
-			//{
-			//	questionsTexts.Add(questions[i].Text);
-			//	questionsAnswers.Add(questions[i].Answer);
-			//}
-
-			//File.WriteAllLines(textsPath, questionsTexts);
-			//File.WriteAllLines(answersPath, questionsAnswers);
-		}
-	}
+            Save(defaultQuestions);
+        }
+    }
 }
