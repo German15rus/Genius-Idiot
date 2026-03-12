49d6042d74596a1a4d8057f1b69f47d53a589b0d
элина <e.kokoeva06@mail.ru>
2026-03-12 09:18:56 +0300

    varerw

--- Genius&Idiot.TgBot/ProgramTG.cs
+++ Genius&Idiot.TgBot/ProgramTG.cs
@@ -1,52 +1,78 @@
 ﻿using Genius___Idiot;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Genius_Idiot.TgBot
 {
-    internal class ProgramTG
-    {
-        static QuestionsStorage questionsStorage = new QuestionsStorage();
-        static TelegramBotClient bot = new TelegramBotClient("8519487868:AAG89J-nRMF1hOeeiYCL2bb7nHi7O5JEqmY");
-        static int randomIndex;
-        static async Task Main(string[] args)
-        {
+	internal class ProgramTG
+	{
+		static UsersStorage usersStorage = new UsersStorage();
+		static QuestionsStorage questionsStorage = new QuestionsStorage();
+		static TelegramBotClient bot = new TelegramBotClient("8519487868:AAG89J-nRMF1hOeeiYCL2bb7nHi7O5JEqmY");
+		static int randomIndex;
+		static bool GameOn = false;
+		static async Task Main(string[] args)
+		{
 
-            var me = await bot.GetMe();
-            Console.WriteLine($"Bot Name - {me.FirstName}.");
+			var me = await bot.GetMe();
+			Console.WriteLine($"Bot Name - {me.FirstName}.");
 
-            bot.OnUpdate += Bot_OnUpdate;
+			bot.OnUpdate += Bot_OnUpdate;
 
-            Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 
-        private static async Task Bot_OnUpdate(Telegram.Bot.Types.Update update)
-        {
-            List<Question> questions = questionsStorage.GetAll();
-            Random rnd = new Random();
+		private static async Task Bot_OnUpdate(Telegram.Bot.Types.Update update)
+		{
+			List<Question> questions = questionsStorage.GetAll();
+			Random rnd = new Random();
 
 
 
-            if (update.Message.Text == "/start")
-            {
-                randomIndex = rnd.Next(questions.Count);
+			if (update.Message.Text == "/start")
+			{
+				await bot.SendMessage(update.Message.Chat.Id, "Выберите :",
+					replyMarkup: new KeyboardButton[][] { ["Начать игру", "Результаты"], ["Добавить вопрос"] });
 
-                await bot.SendMessage(update.Message.Chat.Id, $"{questions[randomIndex].Text}");
-            }
-            
-            else
-            {
-                if (update.Message.Text == $"{questions[randomIndex].Answer}")
-                {
-                    await bot.SendMessage(update.Message.Chat.Id, "Правильно");
-                }
-                else
-                {
-                    bot.SendMessage(update.Message.Chat.Id, $"правильный ответ - {questions[randomIndex].Answer}");
-                    await bot.SendMessage(update.Message.Chat.Id, "Неправильно");
-                }
-            }
-            
-        }
-    }
+			}
+
+			if (update.Message.Text == "Начать игру")
+			{
+				GameOn = true;
+				
+				if (GameOn = true)
+				{
+					if (GameOn == true)
+						randomIndex = rnd.Next(questions.Count);
+					await bot.SendMessage(update.Message.Chat.Id, $"{questions[randomIndex].Text}");
+
+
+
+					if (update.Message.Text == $"{questions[randomIndex].Answer}")
+					{
+						await bot.SendMessage(update.Message.Chat.Id, "Правильно");
+					}
+
+					else
+					{
+						bot.SendMessage(update.Message.Chat.Id, $"правильный ответ - {questions[randomIndex].Answer}");
+						await bot.SendMessage(update.Message.Chat.Id, "Неправильно");
+					}
+				}
+			}
+
+			if (update.Message.Text == "Результаты")
+			{
+				var ress = "<pre>";
+				ress += $"{"Имя",-30}{"Процент %",-30}{"Диагноз",-30}\n";
+
+				foreach (var user in questionsStorage.GetAll())
+				{
+
+				}
+
+			}
+		}
+	}
 }
